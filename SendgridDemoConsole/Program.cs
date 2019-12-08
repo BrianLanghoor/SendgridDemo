ddf3544c38cf22f1e4f57db3de626399b6cdf804
Brian Langhoor <brian.langhoor@infocaster.net>
2019-12-08 16:52:01 +0100

    Set correct begin situation

--- SendgridDemoConsole/Program.cs
+++ SendgridDemoConsole/Program.cs
@@ -16,51 +16,51 @@ namespace SendgridDemo
 
         static async Task Main(string[] args)
         {
-            //var msg = new SendGridMessage();
+            var msg = new SendGridMessage();
 
-            //msg.SetFrom(new EmailAddress("brian_langhoor@hotmail.com", "Kees"));
+            msg.SetFrom(new EmailAddress("brian_langhoor@hotmail.com", "Kees"));
 
-            //var recipients = new List<EmailAddress>
-            //{
-            //    new EmailAddress("brian.langhoor@teamrockstars.nl", "Brian Langhoor"),
-            //};
+            var recipients = new List<EmailAddress>
+            {
+                new EmailAddress("brian.langhoor@teamrockstars.nl", "Brian Langhoor"),
+            };
 
-            //msg.AddTos(recipients);
+            msg.AddTos(recipients);
 
-            //msg.SetSubject("Mail opgesteld vanuit code");
+            msg.SetSubject("Mail opgesteld vanuit code");
 
-            ////msg.AddContent(MimeType.Text, "Hello World plain text!");
-            //msg.AddContent(MimeType.Html, "<p>Hello World!</p>");
+            //msg.AddContent(MimeType.Text, "Hello World plain text!");
+            msg.AddContent(MimeType.Html, "<p>Hello World!</p>");
 
-            //var response = await Client.SendEmailAsync(msg);
+            var response = await Client.SendEmailAsync(msg);
 
-            var requestmessage = new HttpRequestMessage(HttpMethod.Post, "https://api.sendgrid.com/v3/mail/send");
+            //var requestmessage = new HttpRequestMessage(HttpMethod.Post, "https://api.sendgrid.com/v3/mail/send");
 
-            requestmessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", Apikey);
-            requestmessage.Content = new StringContent(
-                "{\n  " +
-                    "\"personalizations\": [\n    " +
-                        "{\n      " +
-                            "\"to\": [\n        " +
-                                "{\n          " +
-                                    "\"email\": \"brian.langhoor@teamrockstars.nl\",\n          " +
-                                    "\"name\": \"Brian Langhoor\"\n        " +
-                                "}\n      " +
-                            "],\n      " +
-                            "\"subject\": \"Hello, World!\"\n    " +
-                        "}\n  " +
-                    "],\n  " +
-                    "\"from\": {\n    " +
-                        "\"email\": \"brian_langhoor@hotmail.com\",\n    " +
-                        "\"name\": \"Brian Langhoor\"\n  " +
-                    "},\n  " +
-                    "\"template_id\": \"<<TemplateId hier>>\"\n" +
-                "}",
-                Encoding.UTF8,
-                "application/json"
-                );
+            //requestmessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", Apikey);
+            //requestmessage.Content = new StringContent(
+            //    "{\n  " +
+            //        "\"personalizations\": [\n    " +
+            //            "{\n      " +
+            //                "\"to\": [\n        " +
+            //                    "{\n          " +
+            //                        "\"email\": \"brian.langhoor@teamrockstars.nl\",\n          " +
+            //                        "\"name\": \"Brian Langhoor\"\n        " +
+            //                    "}\n      " +
+            //                "],\n      " +
+            //                "\"subject\": \"Hello, World!\"\n    " +
+            //            "}\n  " +
+            //        "],\n  " +
+            //        "\"from\": {\n    " +
+            //            "\"email\": \"brian_langhoor@hotmail.com\",\n    " +
+            //            "\"name\": \"Brian Langhoor\"\n  " +
+            //        "},\n  " +
+            //        "\"template_id\": \"<<TemplateId hier>>\"\n" +
+            //    "}",
+            //    Encoding.UTF8,
+            //    "application/json"
+            //    );
 
-            var response = await Client.MakeRequest(requestmessage);
+            //var response = await Client.MakeRequest(requestmessage);
 
             Console.WriteLine(response.StatusCode);
         }
